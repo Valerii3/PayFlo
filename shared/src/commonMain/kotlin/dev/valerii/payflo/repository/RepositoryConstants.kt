e987a59d2982b36ec7b5a2454ac0dbe1601f3918
Valerii <vallerus55@gmail.com>
2025-01-15 20:43:08 +0100

    online multiplatofrm

--- shared/src/commonMain/kotlin/dev/valerii/payflo/repository/RepositoryConstants.kt
+++ shared/src/commonMain/kotlin/dev/valerii/payflo/repository/RepositoryConstants.kt
@@ -1,12 +1,8 @@
 package dev.valerii.payflo.repository
 
-import io.ktor.util.encodeBase64
 
-object RepositoryConstants {
-    // "http://0.0.0.0:8080"
-    // "http://10.0.2.2:8080"
-    const val BASE_URL = "http://10.0.2.2:8080"
-    const val KEY_USER_ID = "user_id"
+expect object RepositoryConstants {
+    val BASE_URL: String
+    val KEY_USER_ID: String
 
-    fun base64Encode(bytes: ByteArray) = bytes.encodeBase64()
 }
---
+++ shared/src/iosMain/kotlin/dev/valerii/payflo/repository/RepositoryConstants.kt
@@ -0,0 +1,6 @@
+package dev.valerii.payflo.repository
+
+actual object RepositoryConstants {
+    actual val BASE_URL: String ="http://0.0.0.0:8080"  // iOS simulator URL
+    actual val KEY_USER_ID: String = "user_id"
+}
